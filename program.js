458a8970e3dc9926713088551f33682efad45495
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2015-01-05 11:55:28 -0600

    Lessons #2, #3 and #4 solved.

--- program.js
+++ program.js
@@ -1,3 +1,49 @@
+// lesson #4
+// static
+
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(process.argv[3]));
+
+app.listen(process.argv[2]);
+
+
+// lesson #3
+// good old form
+
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+var urlencodedParser = bodyparser.urlencoded({extended: false});
+
+app.post('/form', urlencodedParser, function(req, res) {
+	res.end(req.body.str.split('').reverse().join(''));
+})
+
+app.listen(process.argv[2]);
+
+
+// lesson #2
+// jade
+
+var express = require('express');
+// var path = require('path');
+var app = express();
+
+app.set('views', process.argv[3]);
+app.set('view engine', 'jade')
+
+app.get('/home', function(req, res) {
+	res.render('index', {date: new Date().toDateString()});
+})
+
+app.listen(process.argv[2]);
+
+
 // lesson #1
 // hello world
 
