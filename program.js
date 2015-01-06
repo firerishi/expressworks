b25ce249cecc328fddb8545782135a225dbc89a5
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2015-01-06 14:34:57 -0600

    Lessons #7 and #8 on query and json solved.

--- program.js
+++ program.js
@@ -1,3 +1,39 @@
+// lesson #8
+// json me
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.get('/books', function(req, res) {
+	fs.readFile(process.argv[3], function(err, data) {
+		if (err) return res.send(500);
+
+		try {
+			books = JSON.parse(data);
+		} catch (e) {
+			res.send(500);
+		}
+
+		res.json(books);
+	})
+})
+
+app.listen(process.argv[2]);
+
+
+// lesson #7
+// what's in query
+
+var express = require('express');
+var app = express();
+
+app.get('/search', function(req, res) {
+	res.send(req.query);
+})
+
+app.listen(process.argv[2]);
+
+
 // lesson #6
 // param pam pam
 
